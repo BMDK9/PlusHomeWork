410ed0ab5c9c69dd4e036158fc33544c99051b10
BMDK9 <qnrakdlzm77@naver.com>
2023-12-13 00:30:01 +0900

    12/12 회원가입 API

--- src/main/java/com/example/practice/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/example/practice/domain/user/exception/UserErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-    ALREADY_EXSIST("이미 사용하고 있는 아이디입니다.", HttpStatus.BAD_REQUEST),
+    ALREADY_EXSIST("중복된 닉네임입니다.", HttpStatus.BAD_REQUEST),
     ANOTHER_PASSWORD("다른 비밀번호를 사용해주세요.", HttpStatus.BAD_REQUEST),
     DIFFERENT_PASSWORD("입력하신 비밀번호가 다릅니다", HttpStatus.BAD_REQUEST);
 
