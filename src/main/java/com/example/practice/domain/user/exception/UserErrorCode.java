a8d083d1e3681104bf107d066db3554a0a7764fd
BMDK9 <qnrakdlzm77@naver.com>
2023-12-22 16:30:40 +0900

    사소한 수정

--- src/main/java/com/example/practice/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/example/practice/domain/user/exception/UserErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
 
     ALREADY_EXSIST("중복된 닉네임입니다.", HttpStatus.BAD_REQUEST),
-    ANOTHER_PASSWORD("다른 비밀번호를 사용해주세요.", HttpStatus.BAD_REQUEST),
+    ANOTHER_PASSWORD("닉네임이 포함되지 않은 비밀번호를 사용해주세요.", HttpStatus.BAD_REQUEST),
     DIFFERENT_PASSWORD("입력하신 비밀번호가 다릅니다", HttpStatus.BAD_REQUEST),
     WRONG_LOGIN("닉네임 또는 패스워드를 확인해주세요", HttpStatus.FORBIDDEN);
 
