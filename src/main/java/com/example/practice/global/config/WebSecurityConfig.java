2f7568bb0aee82ea5b22f60971f8a43b4669be26
BMDK9 <qnrakdlzm77@naver.com>
2023-12-22 17:05:51 +0900

    swagger 적용

--- src/main/java/com/example/practice/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/practice/global/config/WebSecurityConfig.java
@@ -63,6 +63,7 @@ public class WebSecurityConfig {
                 .requestMatchers("/api/users/login").permitAll() // '/api/users/'로 시작하는 요청 모두 접근 허가
                 .requestMatchers("/api/users/check-name").permitAll() // '/api/users/'로 시작하는 요청 모두 접근 허
                 .requestMatchers("/api/posts/**").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
