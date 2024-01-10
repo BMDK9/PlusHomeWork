1da583500376c80e39c0c786dc740227a836990b
BMDK9 <qnrakdlzm77@naver.com>
2024-01-11 00:48:26 +0900

    refactor: CI 연습 9

--- src/main/java/com/example/practice/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/practice/domain/comment/controller/CommentController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/posts/{postId}/comments")
 public class CommentController {
 
+
     private final postCommentMatcherService postCommentMatcherService;
 
     @PostMapping
